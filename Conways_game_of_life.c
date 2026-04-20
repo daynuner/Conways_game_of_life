0b256836643d5588b7f11ba8db9cf1892653009d
student <your_email@whatever.com>
2026-04-20 11:34:35 +1000

    refactor code, add commits

--- Conways_game_of_life.c
+++ Conways_game_of_life.c
@@ -1,65 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <time.h>
 
-#define W 30
+#define W 40
 #define H 25
 
-int main(void) {
-    int g[H][W] = {0};
-    int next[H][W] = {0};
-    int i, j, x, y, n;
+int main() {
+    int i, j, x, y, n, g[H][W], next[H][W];
+    // Рандомное заполнение
+    for (i = 0; i < H * W; i++) g[i / W][i % W] = rand() % 5 == 0;
 
-    srand((unsigned)time(NULL));
-
-    for (i = 0; i < H; i++) {
-        for (j = 0; j < W; j++) {
-            g[i][j] = (rand() % 5 == 0);
-        }
-    }
-
-    printf("\033[?25l");
-    printf("\033[2J");
-
+    printf("\033[?25l"); // Скрыть курсор
     while (1) {
-        printf("\033[H");
-
+        printf("\033[H"); // В начало экрана
         for (i = 0; i < H; i++) {
             for (j = 0; j < W; j++) {
                 n = 0;
-                for (y = -1; y <= 1; y++) {
-                    for (x = -1; x <= 1; x++) {
-                        if (x || y) {
-                            int ii = (i + y + H) % H;
-                            int jj = (j + x + W) % W;
-                            n += g[ii][jj] > 0;
-                        }
-                    }
-                }
+                // Считаем соседей
+                for (y = -1; y <= 1; y++)
+                    for (x = -1; x <= 1; x++)
+                        if (x || y) n += g[(i + y + H) % H][(j + x + W) % W] > 0;
 
+                // Правила жизни и инкремент цвета (возраста)
                 if (g[i][j] > 0) next[i][j] = (n == 2 || n == 3) ? g[i][j] + 1 : 0;
                 else next[i][j] = (n == 3) ? 1 : 0;
 
-                if (g[i][j] > 0) {
-                    int color = (g[i][j] > 10) ? 196 : 46 + g[i][j] * 10;
-                    if (color > 255) color = 255;
-                    printf("\033[38;5;%dm█", color);
-                } else {
-                    putchar(' ');
-                }
+                // Вывод: цвет меняется от значения в массиве (g[i][j])
+                if (g[i][j]) printf("\033[38;5;%dm█", g[i][j] > 10 ? 196 : 46 + g[i][j] * 10);
+                else printf(" ");
             }
-            putchar('\n');
-            
+            puts("");
         }
-
-        for (i = 0; i < H; i++) {
-            for (j = 0; j < W; j++) {
-                g[i][j] = next[i][j];
-            }
-        }
-        usleep(60000);
+        for (i = 0; i < H * W; i++) g[i / W][i % W] = next[i / W][i % W];
+        usleep(30000); 
     }
-
-    return 0;
 }
